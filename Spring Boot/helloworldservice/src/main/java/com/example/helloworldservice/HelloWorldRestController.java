2e709f149be18f61e92b438acf1eb41da14ee707
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2023-06-10 01:15:18 -0400

    New Spring demo project initialized!!

---
+++ Spring Boot/helloworldservice/src/main/java/com/example/helloworldservice/HelloWorldRestController.java
@@ -0,0 +1,12 @@
+package com.example.helloworldservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+	@GetMapping(value = "/hello")
+	public String sayHello() {
+		return "Hello, Welcome to course on Microservices";
+	}
+}
